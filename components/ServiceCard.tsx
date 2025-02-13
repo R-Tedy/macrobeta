f6495916a08cefd7fcc3d5795ad551e63b5f5c58
Roland Stedy <stedyroland@gmail.com>
2025-02-14 01:09:50 +0300

    services update

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -7,29 +7,25 @@ function ServiceCard({label, serviceImgUrl, serviceDetails}: serviceProps
 ) {
   return (
     <div
-      className='shadow-2xl w-52 h-52 relative border-2 rounded-lg border-primary2'
+      className='shadow-2xl w-52 h-52 relative border-2 rounded-lg border-primary2 cursor-pointer'
     >
-      <Link
-        href='/'
+      <Image
+        src={serviceImgUrl}
+        alt={label}
+        height={200}
+        width={200}
+        className='w-full h-full object-cover hover:opacity-50 rounded-lg'
+      />
+      <div
+        className='absolute bottom-0 right-0 w-52 h-52 flex flex-col justify-center items-center p-1 opacity-0 hover:opacity-100 hover:bg-opacity-70 bg-slate-300 rounded-lg'
       >
-        <Image
-          src={serviceImgUrl}
-          alt={label}
-          height={200}
-          width={200}
-          className='w-full h-full object-cover hover:opacity-50 rounded-lg'
-        />
-        <div
-          className='absolute bottom-0 right-0 w-52 h-52 flex flex-col justify-center items-center p-1 opacity-0 hover:opacity-100 hover:bg-opacity-50 bg-slate-300 rounded-lg'
-        >
-          <h3 className='subtitle_2 text-primary2'>
-            {label}
-          </h3>
-          <p className='paragraphText'>
-            {serviceDetails}
-          </p>
-        </div>
-      </Link>
+        <h3 className='subtitle_2 text-primary2'>
+          {label}
+        </h3>
+        <p className='paragraphText'>
+          {serviceDetails}
+        </p>
+      </div>
     </div>
   )
 }
